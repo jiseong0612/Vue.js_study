ce4efc2853de6230ecd05abb5c0cd99f84236b20
jiseong0612 <hth5545@gmail.com>
2024-02-05 01:19:44 +0900

    페이지 css

--- vue-news/src/components/ToolBar.vue
+++ vue-news/src/components/ToolBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <router-link to="/news">news</router-link> |
-    <router-link to="/ask">ask</router-link> |
-    <router-link to="/jobs">jobs</router-link> |
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
+    <router-link to="/jobs">Jobs</router-link> |
   </div>
 </template>
 
--- vue-news/src/view/AskView.vue
+++ vue-news/src/view/AskView.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <p v-for="(item, i ) in askInfo" :key="i">
-      <!-- <a :href="item.url">{{item.title}}</a> -->
-      <router-link :to="`/item/${item.id}`">{{item.title}}</router-link>
-      <small>
-        {{item.time_ago}} by {{item.user}}
-      </small>
-    </p>
+      <ul class="news-list">
+        <li  v-for="(item, i ) in askInfo" :key="i" class="post">
+          <div class="points">
+            {{item.points}}
+          </div>
+          <div>
+            <p class="news-title">
+              <a :href="item.url">{{item.title}}</a>
+            </p>
+            <small class="link-text">
+              {{item.time_ago}} by
+              <router-link class="link-text" :to="`/item/${item.id}`">{{item.user}}</router-link>
+            </small>
+          </div>
+        </li>
+      </ul>
   </div>
 </template>
 
@@ -23,5 +32,29 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.news-list{
+  margin: 0;
+  padding: 0;
+}
+.post{
+  list-style: none;
+  display: flex;
+  align-items: center;;
+  border-bottom: 1px solid #eee;
+}
+.points{
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title{
+  margin: 0;
+}
+.link-text{
+  color: #828282;
+}
 </style>
